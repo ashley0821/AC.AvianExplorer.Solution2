66a5e4161f6b904357c29e434c8542df35a5183a
ispan <ispan@DESKTOP-PAKSETB>
2023-05-12 13:33:39 +0800

    20230512

---
+++ AC.AvianExplorer.DataLayer/Dtos/RecordDto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AC.AvianExplorer.DataLayer.Dtos
+{
+	public class RecordDto
+	{
+		public int RecordId { get; set; }
+		public int UserId { get; set; }
+		public string LocationName { get; set; }
+		public string FamilyName { get; set; }
+		public string CommonName { get; set; }
+		public DateTime RecordTime { get; set; }
+		public int Quantity { get; set; }
+	}
+
+	public class RecordAddDto
+	{
+		public int UserId { get; set; }
+		public string LocationName { get; set; }
+		public string FamilyName { get; set; }
+		public string CommonName { get; set; }
+		public DateTime RecordTime { get; set; }
+		public int Quantity { get; set; }
+	}
+
+	public class RecordEditDto
+	{
+		public int RecordId { get; set; }
+		public int UserId { get; set; }
+		public string LocationName { get; set; }
+		public string FamilyName { get; set; }
+		public string CommonName { get; set; }
+		public DateTime RecordTime { get; set; }
+		public int Quantity { get; set; }
+	}
+}
---
+++ AC.AvianExplorer.DataLayer/Dtos/SpeciesDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AC.AvianExplorer.DataLayer.Dtos
+{
+	public class SpeciesDto
+	{
+        public int SpeciesId { get; set; }
+		public string CommonName { get; set;}
+		public string SpeciesName { get; set;}
+        public string FamilyName { get; set; }
+    }
+
+	public class SpeciesAddDto
+	{
+		public string CommonName { get; set; }
+		public string SpeciesName { get; set; }
+		public string FamilyName { get; set; }
+	}
+
+	public class SpeciesEditDto
+	{
+		public int SpeciesId { get; set; }
+		public string CommonName { get; set; }
+		public string SpeciesName { get; set; }
+		public string FamilyName { get; set; }
+	}
+}
